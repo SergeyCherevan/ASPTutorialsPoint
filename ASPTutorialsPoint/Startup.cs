d2a2c672856bdd8a66690a4873b7790e4fbc89c8
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-02-24 17:54:03 +0200

    отредаченный вариант загрузки  json в конфигурацию

--- ASPTutorialsPoint/Startup.cs
+++ ASPTutorialsPoint/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,28 +13,28 @@ namespace ASPTutorialsPoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("AppSetting.json");
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. 
+        // Use this method to add services to the container. 
+        // For more information on how to configure your application, 
+        // visit http://go.microsoft.com/fwlink/?LinkID=398940 
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            app.Run(async (context) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                string msg = Configuration["message"];
+                await context.Response.WriteAsync(msg);
             });
         }
     }
