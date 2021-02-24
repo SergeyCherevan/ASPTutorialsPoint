c102e73c5935a947a4b483923749383b200f9b83
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-02-24 16:04:43 +0200

    Откат к началу

--- ASPTutorialsPoint/Startup.cs
+++ ASPTutorialsPoint/Startup.cs
@@ -5,10 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ASPTutorialsPoint
 {
@@ -21,18 +19,21 @@ namespace ASPTutorialsPoint
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app,
-         IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.Run(async (context) => {
-                await context.Response.WriteAsync(
-                   "Hello World! This ASP.NET Core Application");
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
